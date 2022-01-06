a01dfedcb85f217b1c4442612620cb4a14c54216
ItsPixel <85267024+itshdpog@users.noreply.github.com>
2022-01-06 07:00:22 +0100

    added dialogue and fixed modchart

---
+++ assets/preload/data/overbearing/scipt.lua
@@ -0,0 +1,50 @@
+function start (song)
+	print("Song: " .. song .. " @ " .. bpm .. " donwscroll: " .. downscroll)
+end
+
+
+function update (elapsed) -- example https://twitter.com/KadeDeveloper/status/1382178179184422918
+	local currentBeat = (songPos / 1000)*(bpm/60)
+		for i=0,7 do
+			setActorX(_G['defaultStrum'..i..'X'] + 5 * math.sin((currentBeat + i*0.25) * math.pi), i)
+			setActorY(_G['defaultStrum'..i..'Y'] + 5 * math.cos((currentBeat + i*0.25) * math.pi), i)
+		end
+end
+
+function beatHit (beat)
+   -- do nothing
+end
+
+function stepHit (step)
+	-- do nothing
+end
+
+print("Mod Chart script loaded :)")
+
+
+local allowCountdown = false
+function onStartCountdown()
+	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+		startDialogue('dialogue', 'breakfast');
+	end
+end
+
+-- Dialogue (When a dialogue is finished, it calls startCountdown again)
+function onNextDialogue(count)
+	-- triggered when the next dialogue line starts, 'line' starts with 1
+end
+
+function onSkipDialogue(count)
+	-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
+end
